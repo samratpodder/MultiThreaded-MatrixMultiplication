69212279ae9282ee690fa03ad63e08bc53db41bc
samratpodder <iamsamrat@outlook.com>
2021-11-28 13:47:36 +0530

    Working on Single Thread

--- src/SingleThreaded.java
+++ src/SingleThreaded.java
@@ -1,7 +1,51 @@
 package src;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.File;
+
 class SingleThreaded
 {
-    public static int[][] multiply(int[][] mat1 , int[][] mat2){
+    private int order;
+    private int[][] mat1, mat2;
+    private int[][] result;
+    private MatrixReader reader;
+    SingleThreaded(File f1, File f2)throws FileNotFoundException, IOException{
+        reader = new MatrixReader(f1, f2);
+
+    }
+
+    SingleThreaded(int order, int[][] mat1, int[][] mat2)
+    {
+        this.order = order;
+        this.mat1 = mat1;
+        this.mat2 = mat2;
+    }
+    public void multiply(){
+        for (int i = 0; i < mat1.length; i++) {
+            for (int j = 0; j < mat1.length; j++) {
+                for (int k = 0; k < mat1.length; k++) {
+                    result[i][j] += mat1[i][k] * mat2[k][j];
+                }
+            }
+        }
+    }
+    public int[][] getResult(){
+        return result;
+    }
+    public int getOrder(){
+        return order;
+    }
+}
+class MatrixReader{
+    int[][] mat1,mat2;
+    FileReader fr1,fr2;
+    MatrixReader(File f1, File f2) throws FileNotFoundException,IOException{
+        fr1 = new FileReader(f1);
+        fr2 = new FileReader(f2);
+    }
+    public void readMatrix() throws IOException{
         
     }
 }
--- src/app.java
+++ src/app.java
@@ -1,7 +1,25 @@
 package src;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 public class app {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args)throws FileNotFoundException,IOException {
+        File file1 = new File("../inputs/matrix1.txt");
+        File file2 = new File("../inputs/matrix5.txt");
+        FileReader fr1=null;
+        FileReader fr2=null;
+        try{
+            fr1 = new FileReader(file1);
+            fr2 = new FileReader(file2);
+        }
+        catch(FileNotFoundException e){
+            System.out.println("File not found\n"+e);
+        }
+        int[][]mat1,mat2;
+        int character;
+        while((character=fr1.read())!=-1){
+            
+        }
     }
 }
